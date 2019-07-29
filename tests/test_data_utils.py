8eef830bc358a5b53657b4f3f925468259642d94
Robert Cooper <Robert.Cooper@lch.com>
2019-07-29 11:43:13 +0100

    Added median

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -1,8 +1,9 @@
-import datetime
+import os
 from unittest import TestCase
 
+from definitions import ROOT_DIR
 from src.data_container import TimeSeries
-from src.data_utils import DataUtils
+from src.data_utils import *
 
 
 class TestDataUtils(TestCase):
@@ -13,24 +14,20 @@ class TestDataUtils(TestCase):
         cls.hdr_last_price = "Last Price"
 
         cls.parsers = {
-            cls.hdr_date: DataUtils.parse_date,
-            cls.hdr_last_price: DataUtils.parse_float
+            cls.hdr_date: parse_date,
+            cls.hdr_last_price: parse_float
         }
         # ToDo: relative paths and os path join!
         cls.data_clean = {
-            "eq": DataUtils.read_csv(r"C:\Users\naked\PycharmProjects\bh\resources\Equity_history_clean.csv",
-                                     cls.parsers),
-            "fx": DataUtils.read_csv(r"C:\Users\naked\PycharmProjects\bh\resources\FX_history_clean.csv", cls.parsers),
-            "ir": DataUtils.read_csv(r"C:\Users\naked\PycharmProjects\bh\resources\InterestRate_history_clean.csv",
-                                     cls.parsers)
+            "eq": read_csv(os.path.join(ROOT_DIR, "Equity_history_clean.csv"), cls.parsers),
+            "fx": read_csv(os.path.join(ROOT_DIR, "FX_history_clean.csv"), cls.parsers),
+            "ir": read_csv(os.path.join(ROOT_DIR, "InterestRate_history_clean.csv"), cls.parsers)
         }
 
         cls.data_raw = {
-            "eq": DataUtils.read_csv(r"C:\Users\naked\PycharmProjects\bh\resources\Equity_history_raw.csv",
-                                     cls.parsers),
-            "fx": DataUtils.read_csv(r"C:\Users\naked\PycharmProjects\bh\resources\FX_history_raw.csv", cls.parsers),
-            "ir": DataUtils.read_csv(r"C:\Users\naked\PycharmProjects\bh\resources\InterestRate_history_raw.csv",
-                                     cls.parsers)
+            "eq": read_csv(os.path.join(ROOT_DIR, "Equity_history_raw.csv"), cls.parsers),
+            "fx": read_csv(os.path.join(ROOT_DIR, "FX_history_raw.csv"), cls.parsers),
+            "ir": read_csv(os.path.join(ROOT_DIR, "InterestRate_raw.csv"), cls.parsers)
         }
 
     def test_read_csv(self):
@@ -40,42 +37,49 @@ class TestDataUtils(TestCase):
         self.assertTrue(isinstance(self.data_clean[self.hdr_last_price][0], float))
 
     def test_parse_date(self):
-        self.assertEqual(DataUtils.parse_date("11/12/2019"), datetime.datetime(2019, 12, 11))
+        self.assertEqual(parse_date("11/12/2019"), datetime.datetime(2019, 12, 11))
 
     def test_parse_float(self):
-        self.assertEqual(DataUtils.parse_float("5.6123"), 5.6123)
+        self.assertEqual(parse_float("5.6123"), 5.6123)
 
     def test_ewma(self):
-        ewma = DataUtils.ew_ma(self.data_clean["eq"][self.hdr_last_price])
+        ewma = ew_ma(self.data_clean["eq"][self.hdr_last_price])
         self.assertEqual(len(ewma), len(self.data_clean["eq"][self.hdr_last_price]))
         self.assertAlmostEqual(ewma[-1], 5319.445113673711, delta=1e-14)
 
     def test_pct_change(self):
-        pct = DataUtils.pct_change(self.data_clean["eq"][self.hdr_last_price])
+        pct = pct_change(self.data_clean["eq"][self.hdr_last_price])
         pass
 
     def test_ew_zsc(self):
-        # ew_zsc = DataUtils.ew_zsc(pc)
+        # ew_zsc = ew_zsc(pc)
         pass
 
     def test_stale_data(self):
         for k, v in self.data_raw.items():
             tr = TimeSeries(v[self.hdr_date], v[self.hdr_last_price])
-            dr = DataUtils.stale_data(tr.prices, tr.dates, datetime.timedelta(weeks=1))
+            dr = stale_data(tr.prices, tr.dates, datetime.timedelta(weeks=1))
             print(str(k) + " raw " + str(len(dr)) + " " + str([tr.dates[i] for i, d in dr]) + " "
                   + str([tr.prices[i] for i, d in dr]))
 
         for k, v in self.data_clean.items():
             tc = TimeSeries(v[self.hdr_date], v[self.hdr_last_price])
-            dc = DataUtils.stale_data(tc.prices, tc.dates, datetime.timedelta(weeks=1))
+            dc = stale_data(tc.prices, tc.dates, datetime.timedelta(weeks=1))
             print(str(k) + " clean " + str(len(dc)) + " " + str([tc.dates[i] for i, d in dc]) + " "
                   + str([tc.prices[i] for i, d in dc]))
 
     def test_ew_var(self):
-        ew_var = DataUtils.ew_var(self.data_clean["eq"][self.hdr_last_price])
+        pass
+        # ew_var = ew_var(self.data_clean["eq"][self.hdr_last_price])
         # self.assertEqual(len(ew_var), len(self.data_clean[self.hdr_last_price]))
         # self.assertAlmostEqual(ew_var[-1], 5319.445113673711, delta=1e-14)
 
+    def test_median(self):
+        pass
+
+    def test_parition(self):
+        pass
+
     def test_forward_fill_na(self):
-        cleaned = DataUtils.forward_fill_na(self.data_raw[self.hdr_last_price])
+        cleaned = forward_fill_na(self.data_raw[self.hdr_last_price])
         self.assertEqual(len([c for c in cleaned if c != c]), 0)
