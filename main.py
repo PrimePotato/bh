c5ed3d0a7e75972bed80d4d238ef2a9215be6d57
Robert Cooper <Robert.Cooper@lch.com>
2019-07-30 11:47:52 +0100

    Default Changelist

--- main.py
+++ main.py
@@ -26,16 +26,29 @@ def clean_data(ts: TimeSeries, mad_pass_thresholds, iqr_pass_thresholds, ewz_pas
         mad_pass = du.fill_returns_outliers(mad_pass, underlying_outliers)
 
 
+def clean_returns(returns, outlier_func, arg_list):
+    rem_na, na_c = du.forward_fill_na(returns, val=0.)
+    outliers = []
+    cleaned = rem_na
+    for n, t in arg_list:
+        return_outliers = outlier_func(cleaned, n, t)
+        underlying_outliers = du.find_first_in_pair(return_outliers)
+        outliers.append(underlying_outliers)
+        cleaned = du.fill_returns_outliers(cleaned, underlying_outliers)
+    return outliers
+
+
 def load_reports():
-    mad_pass_thresholds = [(10, 20), (30, 12), (100, 10)]
+    mad_pass_thresholds = [(10, 20),
+                           # (30, 15), (100, 10)
+                           ]
 
     iqr_pass_thresholds = [
-        (50, 15),
-        # (50, 15)
+        # (50, 15),
     ]
 
     ewz_pass_thresholds = [
-        (0.1, 5.5)
+        # (0.1, 5.5)
     ]
 
     column_parsers = {
@@ -53,29 +66,36 @@ def load_reports():
         no_zeros, zero_count = du.forward_fill_zeros(rem_na)
 
         pc = du.pct_change(no_zeros)
-        pc_rem_na, na_c = du.forward_fill_na(pc, val=0.)
+        mad_outliers = clean_returns(pc, du.outliers_mad, mad_pass_thresholds)
+        iqr_outliers = clean_returns(pc, du.outliers_iqr, iqr_pass_thresholds)
+        ewz_outliers = clean_returns(pc, du.outliers_zcs, ewz_pass_thresholds)
 
-        mad_outliers = []
-        mad_pass = pc_rem_na
-        for n, t in mad_pass_thresholds:
-            return_outliers = du.outliers_mad(mad_pass, n, t)
-            underlying_outliers = du.find_first_in_pair(return_outliers)
-            mad_outliers.append(underlying_outliers)
-            mad_pass = du.fill_returns_outliers(mad_pass, underlying_outliers)
+        all_outliers = [i for s in mad_outliers for i in s]
+        all_outliers += [i for s in iqr_outliers for i in s]
+        all_outliers += [i for s in ewz_outliers for i in s]
 
-        iqr_outliers = []
-        iqr_pass = mad_pass
-        for n, k in iqr_pass_thresholds:
-            return_outliers = du.outliers_iqr(iqr_pass, n, k)
-            iqr_outliers.append(return_outliers)
-            # du.forward_fill_outliers(iqr_pass, outliers)
-
-        ewz_outliers = []
-        ewz_pass = iqr_pass
-        for d, t in ewz_pass_thresholds:
-            return_outliers = du.outliers_zcs(ewz_pass, d, t)
-            ewz_outliers.append(return_outliers)
-            # du.forward_fill_outliers(ewz_pass, outliers)
+        cleaned = du.forward_fill_outliers(no_zeros, all_outliers)
+        # mad_outliers = []
+        # mad_pass = pc_rem_na
+        # for n, t in mad_pass_thresholds:
+        #     return_outliers = du.outliers_mad(mad_pass, n, t)
+        #     underlying_outliers = du.find_first_in_pair(return_outliers)
+        #     mad_outliers.append(underlying_outliers)
+        #     mad_pass = du.fill_returns_outliers(mad_pass, underlying_outliers)
+        #
+        # iqr_outliers = []
+        # iqr_pass = mad_pass
+        # for n, k in iqr_pass_thresholds:
+        #     return_outliers = du.outliers_iqr(iqr_pass, n, k)
+        #     iqr_outliers.append(return_outliers)
+        #     # du.forward_fill_outliers(iqr_pass, outliers)
+        #
+        # ewz_outliers = []
+        # ewz_pass = iqr_pass
+        # for d, t in ewz_pass_thresholds:
+        #     return_outliers = du.outliers_zcs(ewz_pass, d, t)
+        #     ewz_outliers.append(return_outliers)
+        #     # du.forward_fill_outliers(ewz_pass, outliers)
 
         stale_dates = du.stale_data(ts.prices, ts.dates, datetime.timedelta(weeks=1))
 
@@ -100,7 +120,7 @@ def load_reports():
             'ewz_outliers': ewz_outliers,
         }
 
-        pd.Series(ewz_pass).plot(title=name)
+        pd.Series(cleaned).plot(title=name)
         plt.show()
 
         logging.info(name.name + " " + str(issue_count[name]))
